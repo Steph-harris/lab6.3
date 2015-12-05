f77a5baa956d714215eb4c89da80abb24c6050bc
dong hoon kim <donghoonkim1226@gmail.com>
2015-12-05 13:53:25 -0500

    added flicker url & function to generate picture thumbnails.

--- scripts/photoSearch.js
+++ scripts/photoSearch.js
@@ -4,3 +4,27 @@ $(document).ready(function(){
     //code goes here!
   });
 });
+
+	function buildThumbnail(photoData) {
+		var photoUrl = "https://farm" + photoData.farm;
+		photoUrl += ".staticflickr.com/" + photoData.server;
+		photoUrl += "/" + photoData.id;
+		photoUrl += "_" + photoData.secret + ".jpg";
+
+		var colDiv = $("<div>").addClass("col-md-3");
+		var thumbnailDiv = $("<div>").addClass("thumbnail");
+		var photoImg = $("<img>").attr("src", photoUrl);
+		var captionDiv = $("<div").addClass("caption");
+		var picTitle = $("<p>").appened(photoData.title);
+
+		colDiv.append(thumbnailDiv
+			.append(photoImg)
+			.append(captionDiv
+				.append(picTitle)
+			)
+		);
+		
+		return colDiv;
+
+	}
+
